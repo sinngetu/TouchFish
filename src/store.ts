288f36a893963a735e7bfe2022c02492fded3218
lsp <lsp@me.com>
2023-09-18 07:36:37 +0800

    fix: 修复通用接口瞬时多次调用时，产生重复数据的问题

--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,7 @@ import api from './api/common'
 
 export type Media = Map<number, { name: string, domain: string }>
 export type Platform = Map<number, string>
+export type Keyword = { id: number, word: string }
 
 export default class AppStore {
     constructor() { makeAutoObservable(this) }
@@ -11,27 +12,46 @@ export default class AppStore {
     ready: boolean = false
     media: Media = new Map()
     platform: Platform = new Map()
-    keyword: { [type: number]: string[] } = {}
+    keyword: { [type: number]: Keyword[] } = {}
+
+    _media: Promise<Media> | null = null
+    _platform: Promise<Platform> | null = null
+    _keyword: Promise<{ [type: number]: Keyword[] }> | null = null
 
     onReady = () => this.ready = true
-    getMedia = () => this.media.size
-        ? Promise.resolve(this.media)
-        : api.getMedia()
+    getMedia = () => {
+        if (this.media.size) return Promise.resolve(this.media)
+        if (this._media) return this._media
+
+        this._media = api.getMedia()
             .then((data) => data.forEach(({ id, name, domain }) => this.media.set(id, { name, domain })))
             .then(() => this.media)
 
-    getPlatform = () => this.platform.size
-        ? Promise.resolve(this.platform)
-        : api.getPlatform()
+        return this._media
+    }
+
+    getPlatform = () => {
+        if (this.platform.size) return Promise.resolve(this.platform)
+        if (this._platform) return this._platform
+
+        this._platform = api.getPlatform()
             .then((data) => data.forEach(({ id, name }) => this.platform.set(id, name)))
             .then(() => this.platform)
 
-    getKeyword = () => Object.keys(this.keyword).length
-        ? Promise.resolve(this.keyword)
-        : api.getKeyword().then((data) => data.forEach(({ word, type }) => {
+        return this._platform
+    }
+
+    getKeyword = () => {
+        if (Object.keys(this.keyword).length) return Promise.resolve(this.keyword)
+        if (this._keyword) return this._keyword
+
+        this._keyword = api.getKeyword().then((data) => data.forEach(({ id, word, type }) => {
             if (!this.keyword[type])
                 this.keyword[type] = []
 
-            this.keyword[type].push(word)
+            this.keyword[type].push({ id, word })
         })).then(() => this.keyword)
+
+        return this._keyword
+    }
 }
